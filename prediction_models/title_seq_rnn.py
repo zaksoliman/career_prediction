093f3f25cf0bbcc743a837e16306193026f44255
Zack Soliman <zack.soliman@gmail.com>
2017-10-29 19:49:58 -0400

    refactoring model calss

--- prediction_models/title_seq_rnn.py
+++ prediction_models/title_seq_rnn.py
@@ -11,20 +11,18 @@ from time import time
 
 
 class Model:
-    def __init__(self, data, target,
-                 use_dropout=False, keep_prob=0.5, hidden_dim=250, use_attention=False, attention_dim=100,
-                 use_embedding=False, embedding_dim=50, rnn_cell_type='GRU', max_timesteps=31, learning_rate=0.001,
-                 batch_size=50, n_epochs=100, log_interval=200):
 
-        self.data = data
-        self.target = target
-        self.titles_input_data = None
-        self.seq_lengths = None
+    def __init__(self, data_path="/data/rali7/Tmp/solimanz/data/datasets/title_seq.json", use_dropout=False, n_titles=550,
+                 keep_prob=0.5, hidden_dim=250, use_attention=False, attention_dim=100, use_embedding=False,
+                 embedding_dim=50, rnn_cell_type='LSTM', max_timesteps=31, learning_rate=0.001, batch_size=50,
+                 n_epochs=100, log_interval=200, restore=False, store="/data/rali7/Tmp/solimanz/data/models"):
+
         self.log_interval = log_interval
-        self.title_emb_input = None
-
+        self.restore = restore
         self.keep_prob = keep_prob
         self.use_dropout = use_dropout
+        self.n_titles = n_titles
+        self.n_epochs = n_epochs
         self.use_embedding= use_embedding
         self.rnn_cell_type = rnn_cell_type
         self.emb_dim = embedding_dim
@@ -34,59 +32,62 @@ class Model:
         self.lr = learning_rate
         self.use_att = use_attention
         self.att_dim = attention_dim
+        self.data_path = data_path
+        self.store = store
 
-
-        self.data_path = "/data/rali7/Tmp/solimanz/data/datasets/title_seq.json"
-        self.store = "/data/rali7/Tmp/solimanz/data/models"
-
-
-    def predict(self, config):
+    def predict(self):
 
         # Keep probability for the dropout
         self.keep_prob = tf.placeholder(tf.float32)
-        # Our list of job title sequences
-        self.titles_input_data = tf.placeholder(tf.int32, [None, config.step_num], name="titles_input_data")
+        # Our list of job title sequences (padded to max_timesteps)
+        self.titles_input_data = tf.placeholder(tf.int32, [None, self.max_timesteps], name="titles_input_data")
+        # matrix that will hold the length of out sequences
         self.seq_lengths = tf.placeholder(tf.int32, [None], name="seq_lengths")
-        self.target = tf.placeholder(tf.int32, [None])
+        self.labels = tf.placeholder(tf.int32, [None, self.max_timesteps], name="labels")
 
         # Do embedding
         with tf.device("/cpu:0"):
-            if not config.use_emb:
-                title_embedding = tf.Variable(tf.eye(config.job_num), trainable=False)
+            if not self.use_emb:
+                title_embedding = tf.Variable(tf.eye(self.n_titles), trainable=False)
             else:
                 title_embedding = tf.get_variable(name="job_embedding",
-                                                shape=[config.job_num, config.emb_dim],
+                                                shape=[self.n_titles, self.emb_dim],
                                                 dtype=tf.float32,
                                                 initializer=tf.contrib.layers.xavier_initializer(),
                                                 trainable=True)
 
             self.title_emb_input = tf.nn.embedding_lookup(title_embedding, self.titles_input_data)
 
-        if config.rnn_type == 'LSTM':
-            cell = tf.nn.rnn_cell.LSTMCell(self.hidden_dim)
-        elif config.rnn_type == 'RNN':
+        # Decide on out RNN cell type
+        if self.rnn_type == 'GRU':
+            cell = tf.nn.rnn_cell.GRUCell(self.hidden_dim)
+        elif self.rnn_type == 'RNN':
             cell = tf.nn.rnn_cell.BasicRNNCell(self.hidden_dim)
         else:
-            cell = tf.nn.rnn_cell.GRUCell(self.hidden_dim)
+            cell = tf.nn.rnn_cell.LSTMCell(self.hidden_dim)
 
+        # Adding dropout
         if self.use_dropout:
             cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=self.keep_prob)
 
-        self.output, self.state = tf.nn.dynamic_rnn(cell,
+        self.output, self.prev_states = tf.nn.dynamic_rnn(cell,
                                                     self.title_emb_input,
                                                     sequence_length=self.seq_lengths,
                                                     dtype=tf.float32,
                                                     parallel_iterations=1024)
 
-        if config.rnn_type == 'LSTM':
-            self.state = self.state.h
+        #if config.rnn_type == 'LSTM':
+        #    self.state = self.state.h
 
         self.logit = tf.layers.dense(self.state,
-                                     config.job_num,
+                                     self.n_titles,
                                      activation=None,
                                      kernel_initializer=tf.contrib.layers.xavier_initializer(),
                                      bias_initializer=tf.contrib.layers.xavier_initializer())
 
+        return self.logit
+
+    def loss(self):
         # Loss Function
         tvars = tf.trainable_variables()
         self.loss = tf.reduce_mean(
@@ -101,125 +102,89 @@ class Model:
         self.correct_pred = tf.equal(tf.cast(tf.argmax(self.distribution, 1), tf.int32), self.target)
         self.accuracy = tf.reduce_mean(tf.cast(self.correct_pred, tf.float32))
 
-    def loss(self):
-        pass
-
     def train(self):
-        pass
 
-class TaggerConfigEnglish(object):
-    data_path = "/data/rali7/Tmp/solimanz/data/datasets/title_seq.json"
+        title_to_id, train_data, test_data = loader.load_data(self.data_path)
+        self.title_count = len(title_to_id) + 1
 
-    # Hyper params
+        print("Creating batchers")
+        train_batcher = Batcher(self, train_data)
+        test_batcher = Batcher(self, test_data)
 
-    keep_prob = 0.5
-    batch_size = 100
-    job_num = 0
-    hidden_dim = 1024
-    emb_dim = 50
-    encoder = "rnn"  # averaging rnn biRnn
-    rnn_type = 'LSTM'
-    l2_norm = 0.001
-    step_num = 31
-    use_attention = False
-    att_dim = 100
-    lr = 0.001
-    restore = "/data/rali7/Tmp/solimanz/data/models"
-    max_grad_norm = 5
-    n_epochs = 100
-    log_interval = 200
-    max_train_batches = -1
-    use_emb = False
+        # Assume that you have 12GB of GPU memory and want to allocate ~4GB:
+        gpu_config = tf.ConfigProto()
+        gpu_config.gpu_options.allow_growth = True
 
-def train(config):
-    id_to_job, data_train, data_test = loader.load_data(config)
+        with tf.Session(config=gpu_config) as sess:
 
-    config.job_num = len(id_to_job)+1
+            sess.run(tf.global_variables_initializer())
+            saver = tf.train.Saver()
+            # TODO: give a unique path name to save models to
+            path = os.path.join(".ckpt")
+            if self.restore is not None and os.path.isfile(path):
+                saver.restore(sess, path)
+                print("model restored")
 
-    print("Creating batchers")
-    train_batcher = Batcher(config, data_train)
-    test_batcher = Batcher(config, data_test)
+            for e in range(self.n_epochs):
+                avg_cost = 0
+                avg_acc = 0
+                start_time = time()
+                batch = 0
+                for _ in range(train_batcher.max_batch_num):
+                    with tf.device("/cpu:0"):
+                        job_input_data, job_length, targets = train_batcher.next()
 
-    model = Model(config)
+                    cost, _, acc = sess.run([self.loss, self.train_op, self.accuracy],
+                                            {
+                                                self.titles_input_data: job_input_data,
+                                                self.seq_lengths: job_length,
+                                                self.target: target,
+                                                self.keep_prob: self.keep_prob
+                                            })
 
-    # Assume that you have 12GB of GPU memory and want to allocate ~4GB:
-    gpu_config = tf.ConfigProto()
-    gpu_config.gpu_options.allow_growth = True
+                    # Compute average loss
+                    avg_cost += cost
+                    avg_acc += acc
 
-    with tf.Session(config=gpu_config) as sess:
+                    if batch % self.log_interval == 0 and batch > 0:
+                        cur_loss = avg_cost / batch  # config.log_interval
+                        cur_acc = avg_acc / batch
+                        elapsed = time() - start_time
+                        print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:1.5f} | ms/batch {:5.2f} | '
+                              'loss {:5.6f} | acc {:8.2f}'.format(
+                            e, batch, self.max_train_batches / self.batch_size, self.lr,
+                                      elapsed * 1000 / self.log_interval, cur_loss, cur_acc))
 
-        sess.run(tf.global_variables_initializer())
-        saver = tf.train.Saver()
-        path = os.path.join(config.restore, config.encoder +
-                            config.rnn_type +
-                            str(config.hidden_dim) +
-                            str(config.use_emb) +
-                            str(config.use_attention) + ".ckpt")
-        if config.restore is not None and os.path.isfile(path):
-            saver.restore(sess, path)
-            print("model restored")
+                        start_time = time()
 
-        for e in range(config.n_epochs):
-            avg_cost = 0.
-            avg_acc = 0.
-            epoch_start_time = time()
-            start_time = time()
-            batch = 0
-            for _ in range(train_batcher.max_batch_num):
-                with tf.device("/cpu:0"):
-                    job_input_data, job_length, target = train_batcher.next()
+                    batch += 1
+                    if self.max_train_batches != -1 and batch == self.max_train_batches:
+                        break
+                print("Epoch:", '%04d' % (e + 1), "cost=", "{:.5f}".format(avg_cost/batch), "Accuracy=", "{:.2f}".format(avg_acc/batch))
 
-                cost, _, acc = sess.run([model.loss, model.train_op, model.accuracy],
-                                        {
-                                            model.titles_input_data: job_input_data,
-                                            model.seq_lengths: job_length,
-                                            model.target: target,
-                                            model.keep_prob: config.keep_prob
-                                        })
+                for _ in range(test_batcher.max_batch_num):
+                    with tf.device("/cpu:0"):
+                        job_input_data, job_length, target = test_batcher.next()
 
-                # Compute average loss
-                avg_cost += cost
-                avg_acc += acc
+                    cost, correct_pred, acc = sess.run([self.loss, self.correct_pred, self.accuracy],
+                                         {self.titles_input_data: job_input_data,
+                                          self.seq_lengths: job_length,
+                                          self.target: target,
+                                          self.keep_prob: 1.0})
 
-                if batch % config.log_interval == 0 and batch > 0:
-                    cur_loss = avg_cost / batch  # config.log_interval
-                    cur_acc = avg_acc / batch
-                    elapsed = time() - start_time
-                    print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:1.5f} | ms/batch {:5.2f} | '
-                          'loss {:5.6f} | acc {:8.2f}'.format(
-                        e, batch, config.max_train_batches // config.batch_size, config.lr,
-                                  elapsed * 1000 / config.log_interval, cur_loss, cur_acc))
+                if e % 10 == 0:
+                    save_path = saver.save(sess, os.path.join(self.restore, self.encoder +
+                                                              self.rnn_type +
+                                                              str(self.hidden_dim) +
+                                                              str(self.use_emb) +
+                                                              str(self.use_attention) + ".ckpt"))
+                    print("model saved in file: %s" % save_path)
 
-                    start_time = time()
-
-                batch += 1
-                if config.max_train_batches != -1 and batch == config.max_train_batches:
-                    break
-            print("Epoch:", '%04d' % (e + 1), "cost=", "{:.5f}".format(avg_cost/batch), "Accuracy=", "{:.2f}".format(avg_acc/batch))
-
-            for _ in range(test_batcher.max_batch_num):
-                with tf.device("/cpu:0"):
-                    job_input_data, job_length, target = test_batcher.next()
-
-                cost, correct_pred, acc = sess.run([model.loss, model.correct_pred, model.accuracy],
-                                     {model.titles_input_data: job_input_data,
-                                      model.seq_lengths: job_length,
-                                      model.target: target,
-                                      model.keep_prob: 1.0})
-
-            if e % 10 == 0:
-                save_path = saver.save(sess, os.path.join(config.restore, config.encoder +
-                                                          config.rnn_type +
-                                                          str(config.hidden_dim) +
-                                                          str(config.use_emb) +
-                                                          str(config.use_attention) + ".ckpt"))
-                print("model saved in file: %s" % save_path)
-
-            print('Cost on test:', cost, 'Accuracy on test:', acc)
+                print('Cost on test:', cost, 'Accuracy on test:', acc)
 
 def main():
-    config = TaggerConfigEnglish()
-    train(config)
+    seq_model = Model()
+
 
 if __name__ == "__main__":
     main()
