65316f0c28dbd81afca27acf35b4c2d247689e07
Zack Soliman <zack.soliman@gmail.com>
2017-11-01 16:37:27 -0400

    Adds main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from prediction_models.title_seq_rnn import Model
+from helpers.loader import load_data
+
+def main():
+    path = "/data/rali7/Tmp/solimanz/data/dataset/title_seq.json"
+    mapping, train_data, test_data = load_data(path)
+    seq_model = Model(train_data=train_data)
+    seq_model.train()
+
+if __name__ == "__main__":
+    main()
