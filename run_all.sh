b6493da892278ab34794ea308e4a40950a93f5cd
Zack Soliman <zack.soliman@gmail.com>
2018-06-14 16:01:28 -0400

    sync

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python run_models.py -ds top550 -m simple_rnn -r bow -t test
-python run_models.py -ds top550 -m simple_rnn -r jobid -t test
-python run_models.py -ds top550 -m simple_rnn -r fasttext -t test
-#python run_skill_model.py -ds top550 -t test
-#python  multi_label_models.py top550
+#python run_models.py -ds top550 -m simple_rnn -r bow -t test
+#python run_models.py -ds top550 -m simple_rnn -r jobid -t test
+#python run_models.py -ds top550 -m simple_rnn -r fasttext -t test
+python run_skill_model.py -ds top550 -t test
+python  multi_label_models.py top550
 #
 #python run_models.py -ds reduced7000 -m simple_rnn -r bow -t test
 #python run_models.py -ds reduced7000 -m simple_rnn -r jobid -t test
