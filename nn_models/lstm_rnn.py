abe6bc4e2c56b03f210ec96391282e33856a5346
Zack Soliman <zack.soliman@gmail.com>
2018-11-05 15:44:20 -0500

    Clean up for new code

---
+++ nn_models/lstm_rnn.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+class LSTM_RNN(nn.Module):
+
+    def __init__(self):
+        super(LSTM_RNN, self).__init__()
+
+    def forward(self, *input):
+        pass
